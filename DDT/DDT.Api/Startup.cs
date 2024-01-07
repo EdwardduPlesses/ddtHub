ee071ee7f8846f2ff1883594647cb099d02c359d
Edward <EdwardDP@agilebridge.co.za>
2024-01-07 22:28:13 +0200

    CORS

--- DDT/DDT.Api/Startup.cs
+++ DDT/DDT.Api/Startup.cs
@@ -44,7 +44,7 @@ namespace DDT.Api
             {
                 options.AddPolicy(
                     "CorsPolicy",
-                    builder => builder.WithOrigins("http://localhost:5173")
+                    builder => builder.WithOrigins("http://localhost:5173", "https://ddt-hub.netlify.app/")
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials());
